3782a5266b42c3bc991b57676ed43d276f7198d0
nishaa22 <nisha.rani@excellencetechnologies.in>
2022-06-13 14:26:44 +0530

    layout 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Box from './components/Box';
+import "./index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+      <div class="page">
+        <nav>
+            <a href="#" class="nav__item">Layout2</a><a href="#">About</a><a href="#">Services</a><a href="#">Contact</a>
+        </nav>
+        <div class="content">
+            <div class="content__box">
+                <span>
+                    Sample Heading
+                </span>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos culpa nobis architecto
+                    voluptas consequatur ab, cumque quis deserunt repudiandae quaerat excepturi consequuntur modi saepe
+                    sunt doloremque dolorum repellendus adipisci?</p>
+                <button class="call">Call to action!</button>
+            </div>
+            
+            <div class="content__latestBox">
+                <hr/>
+                <span>Latest Features</span>
+                <div class="content__latestBox__item">
+                   <Box/>
+                   <Box/>
+                   <Box/>
+                   <Box/>
+                </div>
+                <hr class="foot"/>
+            </div>
+            
+            <footer>
+                <p>Copyright &copy; Your Website 2014</p>
+            </footer>
+        </div>
     </div>
+   </>
   );
 }
 
